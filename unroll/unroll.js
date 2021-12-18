b5fab1b41d499178694bbe950f99de4d1c37a2dc
John Saville <53898479+JohnTarvis@users.noreply.github.com>
2021-12-17 20:38:06 -0500

    Update unroll.js

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,27 +1,44 @@
-function unroll(squareArray) {
+fnction unroll(squareArray) {
+    let length = squareArray.length;
+    const unrolled = [];
 
-    
+    let loc = {x:0,y:0}
+    let step = {y:1,x:-1}
 
-    // function squareSpiral(i,size){
-    //     // const pi = Math.pi;
-    //     // const elements = squareArray.length ** 2;
-    //     // const maxTurns = 3;
-    //     // let horizontal = 1;
-    //     // let vertical = 0;
-    //     // let max = size;
+    function show(){
+        console.log(`locx: ${loc.x} locy: ${loc.y} `);
+    }
 
-    //     // // for(let count = 0; count < elements; count++){
-    //     // //     const step = (pi * Math.floor(count / max))
-    //     // // }
-
-    //     // const step = pi * Math.floor(i/max);
-
+    for(let count = 0; count < length; count++){
+        // loc.x += step.x;
+        unrolled.push(squareArray[0][count]);
+        show();
+    }
 
 
-    // }
+    loc.x = length - 1;
 
-    
+    for(let line = length; line >= 0; line--){
 
+        for(let count = 1; count < line; count++){
+            loc.y += step.y;
+            unrolled.push(squareArray[loc.y][loc.x]);
+            show();
+        }
+
+        for(let count = 1; count < line; count++){
+            loc.x += step.x;
+            unrolled.push(squareArray[loc.y][loc.x]);
+            show();
+        }
+
+        step.x *= -1;
+        step.y *= -1;
+
+    }
+    return unrolled;
 }
+
+const sarr = [[1,2,3],[4,5,6],[7,8,9]];
 
 module.exports = unroll;
